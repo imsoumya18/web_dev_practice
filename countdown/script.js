f72b2c293b64f779bfa8a5cd1aa3f41618b4371e
soumya <soumyadeep184@gmail.com>
2023-09-03 23:11:05 +0530

    day 2

---
+++ countdown/script.js
@@ -0,0 +1,20 @@
+let clock = document.getElementById("clock");
+
+let i = 5 * 60;
+
+let interval = setInterval(function () {
+  let min = Math.floor(i / 60);
+  let sec = i - min * 60;
+
+  if (min < 10) min = "0" + min.toString();
+  else min = min.toString();
+
+  if (sec < 10) sec = "0" + sec.toString();
+  else sec = sec.toString();
+
+  let text = min.toString() + ":" + sec.toString();
+  clock.innerText = text;
+
+  if (i == 0) clearInterval(interval);
+  i--;
+}, 1000);
